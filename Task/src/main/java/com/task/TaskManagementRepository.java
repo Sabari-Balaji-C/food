dc913990ca0237a2f99933d2bb0daaaee74ff1f8
sabari balaji <162184802+Sabari-Balaji-C@users.noreply.github.com>
2024-09-17 16:31:13 +0530

    Delete Task directory

--- Task/src/main/java/com/task/TaskManagementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskManagementRepository extends JpaRepository<Tasks,Integer>{
-
-}
